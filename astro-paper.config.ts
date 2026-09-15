bbae38cbaea1bf53ce116438faec20cff7d30caa
Vamsee Kanakala <dev@vamsee.in>
2026-09-15 20:19:43 +0530

    Start adding my details

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,14 +2,14 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
-    description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
+    url: "https://vamsee.in",
+    title: "vamsee.in",
+    description: "A blog where I write about my thoughts on reliability, antifragility, and technical leadership.",
+    author: "Vamsee Kanakala",
+    profile: "vamsee.jpg",
     ogImage: "default-og.jpg",
     lang: "en",
-    timezone: "Asia/Bangkok",
+    timezone: "Asia/Kolkata",
     dir: "ltr",
   },
   posts: {
@@ -23,23 +23,22 @@ export default defineAstroPaperConfig({
     showArchives: true,
     showBackButton: true,
     editPost: {
-      enabled: true,
-      url: "https://github.com/satnaing/astro-paper/edit/main/",
+      enabled: false
     },
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    { name: "github", url: "https://github.com/vamsee" },
+    { name: "x", url: "https://x.com/vamsee" },
+    { name: "linkedin", url: "https://www.linkedin.com/in/vamsee-kanakala/" },
+    { name: "mail", url: "mailto:hello@vamsee.in" },
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
     { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
-    { name: "x",        url: "https://x.com/intent/post?url=" },
+    { name: "x", url: "https://x.com/intent/post?url=" },
     { name: "telegram", url: "https://t.me/share/url?url=" },
     { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
-    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+    { name: "mail", url: "mailto:?subject=See%20this%20post&body=" },
   ],
 });
